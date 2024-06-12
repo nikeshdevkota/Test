bc8635c95e995e318ec941d895f0fbc9c874b4eb
nikeshdevkota <nikeshdevkota28@gmail.com>
2024-06-12 20:54:14 +0900

    Github Actions Added on push request for Cypress_Github_Actions branch

--- Cypress/cypress.config.js
+++ Cypress/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+   projectId: "19go9d",
   reporter: 'mochawesome',
   reporterOptions: {
     reportDir: 'cypress/results',
